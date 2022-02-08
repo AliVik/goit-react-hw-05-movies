fbe76fb33dacc62dfdb7873b118cc73939fc992c
AliVik <alvikul1994@gmail.com>
2022-02-08 22:08:10 +0200

    добавляет ссылкам цвет при активности

--- src/styles/NavLinksStyled.jsx
+++ src/styles/NavLinksStyled.jsx
@@ -4,6 +4,9 @@ import { NavLink } from 'react-router-dom';
 export const HeaderLinks = styled(NavLink)`
   text-decoration: none;
   color: #fff;
+  &.active {
+    color: #e65c00;
+  }
 
   &:not(:last-child) {
     margin-right: 15px;
