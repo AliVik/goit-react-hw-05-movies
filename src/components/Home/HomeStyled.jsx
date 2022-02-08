517b5da778d1a76995dc228ca68900f3b991ac35
AliVik <alvikul1994@gmail.com>
2022-02-08 22:07:34 +0200

    добавляет общие стили главной странице

---
+++ src/components/Home/HomeStyled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const HomeHeader = styled.h1`
+  margin: 0;
+`;
+
+export const Wrapper = styled.div`
+  padding-top: 20px;
+  padding-left: 35px;
+  padding-right: 35px;
+`;
