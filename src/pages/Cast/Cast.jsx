7debdb0b89c37b35dd2217ca76c1d53428d60170
AliVik <alvikul1994@gmail.com>
2022-02-10 01:31:57 +0200

    создаёт страницу "в ролях"

---
+++ src/pages/Cast/Cast.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getMovieCast } from 'helpers/requestsToAPI';
+import CastList from 'components/CastList';
+
+export default function Cast() {
+  const { movieId } = useParams();
+  const [casts, setCasts] = useState(null);
+
+  useEffect(() => {
+    if (!movieId) return;
+    async function handleMovieCast() {
+      try {
+        const response = await getMovieCast(movieId);
+
+        setCasts(
+          response.map(el => ({
+            name: el.name,
+            id: el.id,
+            img: el.profile_path,
+            character: el.character,
+          }))
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    handleMovieCast();
+  }, [movieId]);
+  console.log(casts);
+  return <>{casts && <CastList casts={casts} />}</>;
+}
