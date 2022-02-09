99591264314e1bdec90d95d900d431ef6290a694
AliVik <alvikul1994@gmail.com>
2022-02-09 21:34:33 +0200

    корректирует логику главного приложения

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+
 import { Header } from '../styles/HeaderStyled';
 import { HeaderLinks } from '../styles/NavLinksStyled';
-import Home from './Home';
+import HomePage from '../pages/HomePage';
 import Movies from './Movies';
 import MoviesList from './MoviesList/MoviesList';
-import getTrendingMoviesFromAPI from 'helpers/requestsToAPI';
+import MovieDetailsPage from '../pages/MovieDetailsPage/MovieDetailsPage';
 
 export default function App() {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    if (movies.length !== 0) return;
-    async function handleAPIResponse() {
-      try {
-        const response = await getTrendingMoviesFromAPI();
-        setMovies(
-          response.map(movie => ({
-            title: movie.original_title ?? movie.name,
-            id: movie.id,
-          }))
-        );
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    handleAPIResponse();
-  }, [movies]);
-
   return (
     <>
       <Header>
@@ -35,10 +15,14 @@ export default function App() {
         <HeaderLinks to="movies">Movies</HeaderLinks>
       </Header>
       <Routes>
-        <Route path="/" element={<Home />}>
-          <Route index element={<MoviesList movies={movies} />} />
+        <Route path="/" element={<HomePage />}>
+          <Route index element={<MoviesList />} />
         </Route>
-        <Route path="movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+          <Route path="cast" />
+          <Route path="reviews" />
+        </Route>
+        <Route path="/movies" element={<Movies />} />
       </Routes>
     </>
   );
