5acac4e4830f64b158fe49191719a523f5a26192
AliVik <alvikul1994@gmail.com>
2022-02-09 21:29:58 +0200

    переносит страницу из компонентов в pages

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
-import { HomeHeader, Wrapper } from './HomeStyled';
+import { HomeHeader, Wrapper } from './HomePageStyled';
 
-export default function Home() {
+export default function HomePage() {
   return (
     <Wrapper>
       <HomeHeader>Trending today</HomeHeader>
