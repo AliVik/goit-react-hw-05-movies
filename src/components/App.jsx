eec3eb58d514f7f76b9251ec13549a60951a32b3
AliVik <alvikul1994@gmail.com>
2022-02-11 21:37:57 +0200

    добавляет в главное приложение маршруты для орисовки фильмов по запросу

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Cast from '../pages/CastPage';
 import { Header } from '../styles/HeaderStyled';
 import { HeaderLinks } from '../styles/NavLinksStyled';
 import HomePage from '../pages/HomePage';
-import Movies from '../pages/MoviesPage';
+import MoviesPage from '../pages/MoviesPage';
 import MoviesListPage from '../pages/TrendingMoviesPage';
 import MovieDetailsPage from '../pages/MovieDetailsPage';
 import ReviewsPage from '../pages/ReviewsPage';
+import QueryMovieListPage from '../pages/QueryMovieListPage';
 
 export default function App() {
+  const location = useLocation();
+  console.log(location);
   return (
     <>
       <Header>
@@ -23,7 +26,9 @@ export default function App() {
           <Route path="cast" element={<Cast />} />
           <Route path="reviews" element={<ReviewsPage />} />
         </Route>
-        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies" element={<MoviesPage />}>
+          <Route path={`${location.search}`} element={<QueryMovieListPage />} />
+        </Route>
       </Routes>
     </>
   );
