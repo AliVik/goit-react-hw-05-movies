215179556cd962db8e78b62d0ecbd250333b6402
AliVik <alvikul1994@gmail.com>
2022-02-11 21:36:23 +0200

    добавляет логику отрисовки фильмов по запросу

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -5,25 +5,31 @@ import { SearchButton, SearchInput, SearchForm } from './StyledMoviesPage';
 import { IconContext } from 'react-icons';
 import { Wrapper } from '../HomePage/HomePageStyled';
 import { getMovieByQuery } from '../../helpers/requestsToAPI';
+import { useSearchParams } from 'react-router-dom';
+import QueryMovieListPage from '../QueryMovieListPage';
 
 export default function Movies() {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
+  let [searchParams, setSearchParams] = useSearchParams();
 
   const onInputChange = evt => {
     setQuery(evt.target.value);
   };
   const onFormSubmit = async evt => {
     evt.preventDefault();
+    const form = evt.target;
     if (query === '') toast.error('Please, write down a movie');
 
     try {
       const moviesResults = await getMovieByQuery(query);
-      console.log(moviesResults);
+      setSearchParams({ query });
+      setMovies(moviesResults);
     } catch (error) {
       console.log(error);
     } finally {
       setQuery('');
+      form.reset();
     }
   };
 
@@ -45,6 +51,7 @@ export default function Movies() {
           </IconContext.Provider>
         </SearchButton>
       </SearchForm>
+      <QueryMovieListPage movies={movies} />
       <Toaster />
     </Wrapper>
   );
