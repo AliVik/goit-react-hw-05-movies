9f53c28f4ee572f330c31e386c364ace2ecd08dd
AliVik <alvikul1994@gmail.com>
2022-02-13 00:00:35 +0200

    добавляет логику кнопке "Назад"

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react';
 import { IconContext } from 'react-icons';
-import { useParams, Outlet } from 'react-router-dom';
+import { useParams, Outlet, useNavigate, useLocation } from 'react-router-dom';
 import { getMovieById } from '../../helpers/requestsToAPI';
-import { Button, GoBackLink } from './StyledMovieDetailsPage';
+import { Button } from './StyledMovieDetailsPage';
 import { IoArrowBackCircle } from 'react-icons/io5';
 import { Wrapper } from '../HomePage/HomePageStyled';
 import MovieDetails from 'components/MovieDetails/MovieDetails';
-import AdditionalInformation from '../../components/AdditionalInformation/AdditionalInformation';
 
 export default function MovieDetailsPage() {
   const { movieId } = useParams();
   const [movie, setMovie] = useState(null);
+  const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
     async function getMovieData() {
@@ -26,19 +27,24 @@ export default function MovieDetailsPage() {
 
   return (
     <Wrapper>
-      <Button type="button">
-        <GoBackLink to="/">
-          <IconContext.Provider value={{ size: '70px', fill: '#330066' }}>
-            <div>
-              <IoArrowBackCircle />
-            </div>
-          </IconContext.Provider>
-        </GoBackLink>
+      <Button
+        type="button"
+        onClick={() => {
+          return location.pathname !== `/movies/${movieId}`
+            ? navigate(-2)
+            : navigate(-1);
+        }}
+      >
+        <IconContext.Provider value={{ size: '70px', color: '#330066' }}>
+          <div>
+            <IoArrowBackCircle />
+          </div>
+        </IconContext.Provider>
       </Button>
       {movie && (
         <>
           <MovieDetails movie={movie} />
-          <AdditionalInformation />
+
           <Outlet />
         </>
       )}
