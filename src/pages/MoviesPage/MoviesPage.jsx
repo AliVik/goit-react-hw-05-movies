dca6b8576e0963e5a7cbd7a89e63ce68d854be4a
AliVik <alvikul1994@gmail.com>
2022-02-13 00:00:59 +0200

    корректирует логику формы

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,37 +1,45 @@
 import { RiSearchEyeFill } from 'react-icons/ri';
 import { toast, Toaster } from 'react-hot-toast';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { SearchButton, SearchInput, SearchForm } from './StyledMoviesPage';
 import { IconContext } from 'react-icons';
 import { Wrapper } from '../HomePage/HomePageStyled';
 import { getMovieByQuery } from '../../helpers/requestsToAPI';
-import { useSearchParams } from 'react-router-dom';
+import { useLocation, useSearchParams } from 'react-router-dom';
 import QueryMovieListPage from '../QueryMovieListPage';
 
 export default function Movies() {
   const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
   let [searchParams, setSearchParams] = useSearchParams();
+  const location = useLocation();
 
   const onInputChange = evt => {
     setQuery(evt.target.value);
   };
+
   const onFormSubmit = async evt => {
     evt.preventDefault();
     const form = evt.target;
     if (query === '') toast.error('Please, write down a movie');
-
-    try {
-      const moviesResults = await getMovieByQuery(query);
-      setSearchParams({ query });
-      setMovies(moviesResults);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setQuery('');
-      form.reset();
-    }
+    setSearchParams({ query });
+    form.reset();
   };
+
+  useEffect(() => {
+    if (location.search === '') return;
+    const query = searchParams.get('query');
+
+    async function getMoviesByQuery() {
+      try {
+        const moviesResults = await getMovieByQuery(query);
+        setMovies(moviesResults);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getMoviesByQuery();
+  }, [location.search, searchParams]);
 
   return (
     <Wrapper>
