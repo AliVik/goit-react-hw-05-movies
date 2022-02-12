0a40371740126b8d5e86d0dd7d601029968bc0be
AliVik <alvikul1994@gmail.com>
2022-02-12 23:57:40 +0200

    убирает лишний код

--- src/components/AdditionalInformation/AdditionalInformation.jsx
+++ src/components/AdditionalInformation/AdditionalInformation.jsx
@@ -1,23 +1,25 @@
 import {
-  AddInfoWrapper,
   AddInfoLinks,
   AddInfoPointsText,
+  AddInfoLinksWrapper,
 } from './StyledAdditionalInfo';
 import { IoPeopleSharp } from 'react-icons/io5';
 import { MdOutlineRateReview } from 'react-icons/md';
 
 export default function AdditionalInformation() {
   return (
-    <AddInfoWrapper>
+    <>
       <h3>Additional information</h3>
-      <AddInfoLinks to="cast">
-        <IoPeopleSharp />
-        <AddInfoPointsText>Cast</AddInfoPointsText>
-      </AddInfoLinks>
-      <AddInfoLinks to="reviews">
-        <MdOutlineRateReview />
-        <AddInfoPointsText>Reviews</AddInfoPointsText>
-      </AddInfoLinks>
-    </AddInfoWrapper>
+      <AddInfoLinksWrapper>
+        <AddInfoLinks to="cast">
+          <IoPeopleSharp />
+          <AddInfoPointsText>Cast</AddInfoPointsText>
+        </AddInfoLinks>
+        <AddInfoLinks to="reviews">
+          <MdOutlineRateReview />
+          <AddInfoPointsText>Reviews</AddInfoPointsText>
+        </AddInfoLinks>
+      </AddInfoLinksWrapper>
+    </>
   );
 }
