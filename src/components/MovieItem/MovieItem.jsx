6ca42021bf8b084a6eacf20788221083b23fb95e
AliVik <alvikul1994@gmail.com>
2022-02-12 16:46:34 +0200

    добавляет проверку на отсутствие постера

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -2,7 +2,9 @@ import {
   StyledMoviesItem,
   StyledMovieName,
   MovieLink,
-} from '../../pages/TrendingMoviesPage/StyledTrendingMoviesPage';
+  Image,
+} from './StyledMovieItem';
+import noPoster from '../../images/no_poster.jpg';
 
 export default function MovieItem({ props }) {
   const { id, title, poster_path } = props;
@@ -10,10 +12,15 @@ export default function MovieItem({ props }) {
   return (
     <StyledMoviesItem key={id}>
       <MovieLink to={`/movies/${id}`}>
-        <img
-          src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-          alt={title}
-        />
+        {poster_path ? (
+          <Image
+            src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+            alt={title}
+          />
+        ) : (
+          <Image src={noPoster} alt={title} />
+        )}
+
         <StyledMovieName>{title}</StyledMovieName>
       </MovieLink>
     </StyledMoviesItem>
