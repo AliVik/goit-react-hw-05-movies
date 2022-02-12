ec3591c1a148214fb19e3909b552cfe4f12c3d81
AliVik <alvikul1994@gmail.com>
2022-02-12 16:45:47 +0200

    перемещает стили MovieItem

---
+++ src/components/MovieItem/StyledMovieItem.jsx
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Image = styled.img`
+  display: block;
+  width: 350px;
+  height: 500px;
+`;
+
+export const StyledMoviesItem = styled.li`
+  list-style: none;
+  margin-bottom: 20px;
+  margin-right: 20px;
+`;
+
+export const StyledMovieName = styled.span`
+  display: block;
+  margin-top: 10px;
+  text-align: center;
+`;
+
+export const MovieLink = styled(Link)`
+  color: #000;
+  text-decoration: none;
+  transition: font-weight 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  &:hover,
+  &:focus {
+    font-weight: bold;
+  }
+`;
--- src/pages/TrendingMoviesPage/StyledTrendingMoviesPage.jsx
+++ src/pages/TrendingMoviesPage/StyledTrendingMoviesPage.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
 
 export const StyledMoviesList = styled.ul`
   display: flex;
@@ -9,24 +8,4 @@ export const StyledMoviesList = styled.ul`
   margin: 0;
   padding: 0;
   margin-top: 15px;
-`;
-
-export const StyledMoviesItem = styled.li`
-  list-style: none;
-  margin-bottom: 10px;
-`;
-
-export const StyledMovieName = styled.span`
-  display: block;
-  margin-left: 5px;
-`;
-
-export const MovieLink = styled(Link)`
-  color: #000;
-  text-decoration: none;
-  transition: font-weight 250ms cubic-bezier(0.4, 0, 0.2, 1);
-  &:hover,
-  &:focus {
-    font-weight: bold;
-  }
 `;
