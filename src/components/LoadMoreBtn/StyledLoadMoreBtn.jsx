a1356c235be3b3a98badb7ab378536911fdaf945
AliVik <alvikul1994@gmail.com>
2022-02-13 17:44:02 +0200

    добавляет кнопке загрузки фильмов стили

---
+++ src/components/LoadMoreBtn/StyledLoadMoreBtn.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const LoadButton = styled.button`
+  display: block;
+  height: 40px;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin: auto;
+  border: none;
+  border-radius: 5%;
+  background-color: #330066;
+  color: #fff;
+`;
