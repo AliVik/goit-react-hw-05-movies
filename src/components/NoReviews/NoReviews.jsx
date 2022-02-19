50f68c0f34c6b68c0641d4f3992bfffc53b729a3
AliVik <alvikul1994@gmail.com>
2022-02-19 20:09:01 +0200

    корректирует цвет иконки

--- src/components/NoReviews/NoReviews.jsx
+++ src/components/NoReviews/NoReviews.jsx
@@ -1,11 +1,14 @@
 import { NoReviewsText, NoReviewsSpan } from './StyledNoReviews';
 import { ImSad } from 'react-icons/im';
+import { IconContext } from 'react-icons';
 
 export default function NoReviews() {
   return (
     <NoReviewsText>
       <NoReviewsSpan>There is no any review yet </NoReviewsSpan>
-      <ImSad />
+      <IconContext.Provider value={{ color: '#fff' }}>
+        <ImSad />
+      </IconContext.Provider>
     </NoReviewsText>
   );
 }
